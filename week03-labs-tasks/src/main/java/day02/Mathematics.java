69ca183608365f04ea9bafd80c279d0f4fa5374d
AdamZsoldos <adam.zsoldos@reddwarfgames.com>
2021-10-26 15:39:48 +0200

    Added day02.Mathematics

---
+++ week03-labs-tasks/src/main/java/day02/Mathematics.java
@@ -0,0 +1,19 @@
+package day02;
+
+public class Mathematics {
+
+    public static void main(String[] args) {
+        System.out.println("Prime numbers");
+        for (int i = 0; i < 500; i++) {
+            if (isPrime(i)) System.out.println(i);
+        }
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) return false;
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+}
