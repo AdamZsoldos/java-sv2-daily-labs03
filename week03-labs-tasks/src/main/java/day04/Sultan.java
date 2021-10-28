b85d9094a38dff52bf41056e350946bdd3f38a6c
AdamZsoldos <adam.zsoldos@reddwarfgames.com>
2021-10-28 16:05:54 +0200

    Updated day04.Sultan

--- week03-labs-tasks/src/main/java/day04/Sultan.java
+++ week03-labs-tasks/src/main/java/day04/Sultan.java
@@ -8,9 +8,9 @@ public class Sultan {
         for (int i = 1; i < 101; i++) {
             prison.toggleCellLocks(i);
 
-            System.out.print("Day " + i + " open cell indices: ");
+            System.out.print("Day " + i + " open cells: ");
             for (int j = 0; j < prison.getCells().length; j++) {
-                if (prison.getCells()[j]) System.out.print(j + " ");
+                if (prison.getCells()[j]) System.out.print(j + 1 + " ");
             }
             System.out.println();
         }
